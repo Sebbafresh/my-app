bb862a07d2285b89a8d9e0508e332b08dfdaebf5
sebastian <ereon@live.com>
2022-05-12 18:00:10 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  public title = 'my-app';
+  public showTemplate: boolean = false;
+  public randomNumber: number = 0;
+  public tekstStreng: string = "";
+  public maxTall: number = 600000;
+  public naavaerendeTall: number;
+  public personer: {navn: string, tall: number}[] = [{navn: "Eirik", tall: 69}, {navn: "Tor", tall: 420}];
+  public extraTall: string = "5";
+
+
+
+
+  public onClick(){
+    this.personer.push({navn: "Sebastian", tall: 123});
+
+    this.personer.forEach((person) => {
+      console.log("Personen vi ser på nå: ", person.navn, person.tall);
+    });
+
+
+    // for(let startTall: number = 0; startTall <= this.maxTall; startTall++){
+    //   this.naavaerendeTall = startTall;
+    // }
+    
+    // this.randomNumber = Math.random();
+    // if(this.randomNumber < 0.5){
+    //   this.tekstStreng = "Tallet er mindre enn 0,5";
+    // } else {
+    //   this.tekstStreng = "Tallet er mer enn 0,5";
+    // }
+
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { SideAppComponent } from './side-app/side-app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { IpInfoComponent } from './ip-info/ip-info.component';
+import { TestMedEirikComponent } from './test-med-eirik/test-med-eirik.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SideAppComponent,
-    IpInfoComponent
+    IpInfoComponent,
+    TestMedEirikComponent
   ],
   imports: [
     BrowserModule,

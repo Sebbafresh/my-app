36059863344c34bb1ed06293d8d97397e5342e94
sebastian <ereon@live.com>
2022-05-12 18:07:16 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SideAppComponent } from './side-app/side-app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { IpInfoComponent } from './ip-info/ip-info.component';
-import { TestMedEirikComponent } from './test-med-eirik/test-med-eirik.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SideAppComponent,
+    
     IpInfoComponent,
-    TestMedEirikComponent
+    
   ],
   imports: [
     BrowserModule,
